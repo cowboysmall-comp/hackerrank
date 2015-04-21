478edfc03d12eb2fdbf85a8d15ad376fc5091a67
Jerry Kiely <jerry@cowboysmall.com>
2015-04-20 23:20:14 -0400

    added two more solutions to problems in the warmup track

---
+++ src/warmup/sherlock_and_squares.py
@@ -0,0 +1,47 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../tools'))
+
+import files
+
+import math
+
+
+'''
+    submitted code:
+
+    def count_squares(A, B):
+        A2 = math.sqrt(A)
+        A3 = int(A2)
+        return int(math.sqrt(B)) - A3 + (0 if (A2 - A3) > 0 else 1)
+
+
+    def main():
+        T = int(input())
+
+        for i in range(T):
+            A, B = [int(val) for val in input().split()]
+            print(count_squares(A, B))
+
+
+    if __name__ == "__main__":
+        main()
+
+'''
+
+
+def count_squares(A, B):
+    A2 = math.sqrt(A)
+    A3 = int(A2)
+    return int(math.sqrt(B)) - A3 + (0 if (A2 - A3) > 0 else 1)
+
+
+def main(argv):
+    lines = files.read_lines_of_ints(argv[0])
+
+    for line in lines[1:]:
+        print(count_squares(*line))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ tests/warmup/test_sherlock_and_squares.py
@@ -0,0 +1,23 @@
+import unittest
+
+from warmup.sherlock_and_squares import count_squares
+
+
+class TestSherlockAndSquares(unittest.TestCase):
+
+    def test_basic_cases(self):
+        self.assertEqual(count_squares(3, 9), 2)
+        self.assertEqual(count_squares(4, 9), 2)
+        self.assertEqual(count_squares(17, 24), 0)
+        self.assertEqual(count_squares(1, 24), 4)
+        self.assertEqual(count_squares(1, 25), 5)
+        self.assertEqual(count_squares(1, 26), 5)
+        self.assertEqual(count_squares(63, 82), 2)
+        self.assertEqual(count_squares(64, 81), 2)
+        self.assertEqual(count_squares(65, 80), 0)
+        self.assertEqual(count_squares(80, 82), 1)
+        self.assertEqual(count_squares(1, 100), 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
