f11f46ba25c511c45f9d06b0599909f6d0c8b2d1
Jerry Kiely <jerry@cowboysmall.com>
2015-07-23 22:14:35 -0400

    added a solution to a problem in the dynamic programming track

---
+++ src/algorithms/dynamic_programming/equal.py
@@ -0,0 +1,66 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../tools'))
+
+import files
+
+import math
+
+
+'''
+    submitted code:
+
+    def operations(N, A):
+        M = min(A)
+        O = []
+
+        for m in range(M - 4, M + 1):
+            C = 0
+            for i in range(N):
+                T  = A[i] - m
+                C += T // 5 + (T % 5) // 2 + (T % 5) % 2
+            O.append(C)
+
+        return min(O)
+
+
+    def main():
+        T = int(input())
+
+        for _ in range(T):
+            N = int(input())
+            A = [int(i) for i in input().split()]
+            print(operations(N, A))
+
+
+    if __name__ == "__main__":
+        main()
+
+'''
+
+def operations(N, A):
+    M = min(A)
+    O = []
+
+    for m in range(M - 4, M + 1):
+        C = 0
+        for i in range(N):
+            T  = A[i] - m
+            C += T // 5 + (T % 5) // 2 + (T % 5) % 2
+        O.append(C)
+
+    return min(O)
+
+
+def main(argv):
+    lines = files.read_lines_of_ints(argv[0])
+    T     = lines[0][0]
+
+    for i in range(T):
+        N = lines[(2 * i) + 1][0]
+        A = lines[(2 * i) + 2]
+        print(operations(N, A))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
