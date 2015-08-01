c774742f3f5469a63517ab9385f2df98fc7ddc80
Jerry Kiely <jerry@cowboysmall.com>
2015-07-31 22:33:20 -0400

    added another solution to a problem in the dynamic programming track

--- src/algorithms/dynamic_programming/xor_and_sum.py
+++ src/algorithms/dynamic_programming/xor_and_sum.py
@@ -9,12 +9,52 @@ import files
     submitted code:
 
     def xor_and_sum(a, b):
-        return -1
+        C = [[0 for _ in range(2)] for _ in range(1000000)]
+        A = [0 for _ in range(1000000)]
+        B = [0 for _ in range(1000000)]
+        P = [0 for _ in range(1000000)]
+
+
+        L = len(a)
+        M = len(b)
+        for i in range(L):
+            A[L - i - 1] = int(a[i])
+        for i in range(M):
+            B[M - i - 1] = int(b[i])
+
+
+        C[0][0] = (B[0] == 0)
+        C[0][1] = (B[0] == 1)
+        for i in range(1, M + 314159):
+            C[i][0] = C[i - 1][0] + (B[i] == 0)
+            C[i][1] = C[i - 1][1] + (B[i] == 1)
+
+
+        P[0] = 1
+        for i in range(1, M + 314159):
+            P[i]  = P[i - 1] * 2
+            P[i] %= 1000000007
+
+
+        T = P[0] * ((A[0] * (C[0][0] + 314159)) + ((A[0] ^ 1) * C[0][1]))
+        for i in range(1, M + 314159):
+            if i < 314159:
+                T1 = C[i][0] - i + 314159
+                T2 = C[i][1]
+            else:
+                T1 = C[M - 1][0] - C[i - 314159 - 1][0] + 314159
+                T2 = C[M - 1][1] - C[i - 314159 - 1][1]
+
+            T += P[i] * ((A[i] * T1) + ((A[i] ^ 1) * T2))
+            T %= 1000000007
+
+
+        return T
 
 
     def main():
-        a = reversed(input())
-        b = reversed(input())
+        a = input()
+        b = input()
 
         print(xor_and_sum(a, b))
 
@@ -25,7 +65,47 @@ import files
 '''
 
 def xor_and_sum(a, b):
-    return -1
+    C = [[0 for _ in range(2)] for _ in range(1000000)]
+    A = [0 for _ in range(1000000)]
+    B = [0 for _ in range(1000000)]
+    P = [0 for _ in range(1000000)]
+
+
+    L = len(a)
+    M = len(b)
+    for i in range(L):
+        A[L - i - 1] = int(a[i])
+    for i in range(M):
+        B[M - i - 1] = int(b[i])
+
+
+    C[0][0] = (B[0] == 0)
+    C[0][1] = (B[0] == 1)
+    for i in range(1, M + 314159):
+        C[i][0] = C[i - 1][0] + (B[i] == 0)
+        C[i][1] = C[i - 1][1] + (B[i] == 1)
+
+
+    P[0] = 1
+    for i in range(1, M + 314159):
+        P[i]  = P[i - 1] * 2
+        P[i] %= 1000000007
+
+
+    T = P[0] * ((A[0] * (C[0][0] + 314159)) + ((A[0] ^ 1) * C[0][1]))
+    for i in range(1, M + 314159):
+        if i < 314159:
+            T1 = C[i][0] - i + 314159
+            T2 = C[i][1]
+        else:
+            T1 = C[M - 1][0] - C[i - 314159 - 1][0] + 314159
+            T2 = C[M - 1][1] - C[i - 314159 - 1][1]
+
+        T += P[i] * ((A[i] * T1) + ((A[i] ^ 1) * T2))
+        T %= 1000000007
+
+
+    return T
 
 
 def main(argv):
