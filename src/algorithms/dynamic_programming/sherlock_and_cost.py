5888f0b22849f586edcabba46d892bd3d281dd18
Jerry Kiely <jerry@cowboysmall.com>
2015-08-01 22:34:40 -0400

    slight cleanup

--- src/algorithms/dynamic_programming/sherlock_and_cost.py
+++ src/algorithms/dynamic_programming/sherlock_and_cost.py
@@ -12,8 +12,8 @@ import files
         C = [[0 for _ in range(2)] for _ in range(N)]
 
         for i in range(1, N):
-            C[i][0] = max(C[i - 1][1] + abs(B[i - 1] - 1), C[i - 1][0])
-            C[i][1] = max(C[i - 1][0] + abs(B[i] - 1), C[i - 1][1] + abs(B[i] - B[i - 1]))
+            C[i][0] = max(C[i - 1][1] + abs(B[i - 1] - 1),    C[i - 1][0])
+            C[i][1] = max(C[i - 1][1] + abs(B[i] - B[i - 1]), C[i - 1][0] + abs(B[i] - 1))
 
         return max(C[N - 1])
 
@@ -36,8 +36,8 @@ def cost(N, B):
     C = [[0 for _ in range(2)] for _ in range(N)]
 
     for i in range(1, N):
-        C[i][0] = max(C[i - 1][1] + abs(B[i - 1] - 1), C[i - 1][0])
-        C[i][1] = max(C[i - 1][0] + abs(B[i] - 1), C[i - 1][1] + abs(B[i] - B[i - 1]))
+        C[i][0] = max(C[i - 1][1] + abs(B[i - 1] - 1),    C[i - 1][0])
+        C[i][1] = max(C[i - 1][1] + abs(B[i] - B[i - 1]), C[i - 1][0] + abs(B[i] - 1))
 
     return max(C[N - 1])
 
