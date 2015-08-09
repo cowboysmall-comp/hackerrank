7652e46547d2d7d9a04b5eaf27c8591d54b4c2a9
Jerry Kiely <jerry@cowboysmall.com>
2015-08-09 16:26:39 -0400

    completed the greedy track

---
+++ src/algorithms/greedy/chief_hopper.py
@@ -0,0 +1,86 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../tools'))
+
+import files
+
+
+'''
+    submitted code:
+
+    def chief_hopper(N, H):
+
+        def energy(E):
+            for h in H:
+                if h > E:
+                    E -= h - E
+                else:
+                    E += E - h
+
+                if E < 0:
+                    return False
+
+            return True
+
+        def search(L, H):
+            while L < H:
+                M = L + (H - L) // 2
+                if energy(M):
+                    H = M
+                else:
+                    L = M + 1
+
+            return L
+
+        return search(0, 100000)
+
+
+    def main():
+        N = int(input())
+        H = [int(i) for i in input().split()]
+
+        print(chief_hopper(N, H))
+
+
+    if __name__ == "__main__":
+        main()
+
+'''
+
+def chief_hopper(N, H):
+
+    def energy(E):
+        for h in H:
+            if h > E:
+                E -= h - E
+            else:
+                E += E - h
+
+            if E < 0:
+                return False
+
+        return True
+
+    def search(L, H):
+        while L < H:
+            M = L + (H - L) // 2
+            if energy(M):
+                H = M
+            else:
+                L = M + 1
+
+        return L
+
+    return search(0, 100000)
+
+
+def main(argv):
+    lines = files.read_lines_of_ints(argv[0])
+    N     = lines[0][0]
+    H     = lines[1]
+
+    print(chief_hopper(N, H))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ src/algorithms/greedy/team_formation.py
@@ -0,0 +1,88 @@
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../tools'))
+
+import files
+
+import heapq
+
+
+'''
+    submitted code:
+
+    def team_formation(N, A):
+        D = {}
+
+        def insert(K, V):
+            if K in D:
+                D[K].append(V)
+                heapq.heapify(D[K])
+            else:
+                D[K] = [V]
+
+        for a in sorted(A):
+            if a - 1 in D:
+                L = D[a - 1]
+                W = heapq.heappop(L)
+
+                insert(a, W + 1)
+
+                if not L:
+                    del D[a - 1]
+            else:
+                insert(a, 1)
+
+        return min(heapq.heappop(D[d]) for d in D)
+
+
+    def main():
+        T = int(input())
+
+        for _ in range(T):
+            I    = [int(i) for i in input().split()]
+            N, A = I[0], I[1:]
+
+            print(0 if N == 0 else team_formation(N, A))
+
+
+    if __name__ == "__main__":
+        main()
+
+'''
+
+def team_formation(N, A):
+    D = {}
+
+    def insert(K, V):
+        if K in D:
+            D[K].append(V)
+            heapq.heapify(D[K])
+        else:
+            D[K] = [V]
+
+    for a in sorted(A):
+        if a - 1 in D:
+            W = heapq.heappop(D[a - 1])
+
+            insert(a, W + 1)
+
+            if not D[a - 1]:
+                del D[a - 1]
+        else:
+            insert(a, 1)
+
+    return min(heapq.heappop(D[d]) for d in D)
+
+
+def main(argv):
+    lines = files.read_lines_of_ints(argv[0])
+    T     = lines[0][0]
+
+    for line in lines[1:]:
+        N, A  = line[0], line[1:]
+
+        print(0 if N == 0 else team_formation(N, A))
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
